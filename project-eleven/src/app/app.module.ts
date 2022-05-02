869e7ac80894ffd0dd3a4ebfe58e9dd596e4bd28
Sumithra M <sumi3006@global.chainsys.com>
2022-05-02 11:42:13 +0530

    firstCommit

--- project-eleven/src/app/app.module.ts
+++ project-eleven/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { HitButtonComponent } from './hit-button/hit-button.component';
+// import { CounterAppComponent } from './counter-app/counter-app.component';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    EmployeeFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
